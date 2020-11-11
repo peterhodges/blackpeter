33a4fa3bc0a07d6f0cb68aa630b1f3694b9643f7
Peter Hodges <peteredwardhodges@gmail.com>
2020-11-11 17:50:04 +0000

    Add basic socket.io integration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,30 @@
-console.log("Hello");
+import * as http from 'http';
+import * as fs from 'fs'
+import { Socket } from 'socket.io';
+
+const app = http.createServer(handler);
+const io = require('socket.io')(app);
+
+function handler (req: http.IncomingMessage, res: http.ServerResponse) {
+    fs.readFile(__dirname + '/index.html', (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading index.html');
+      }
+
+      res.writeHead(200);
+      res.end(data);
+    });
+}
+
+io.on('connection', (socket: Socket) => {
+    console.log('user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+
+    socket.emit("welcome", "hello world!");
+});
+
+app.listen(3000, () => { console.log('Listening on port 3000')});
