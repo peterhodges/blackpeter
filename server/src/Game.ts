133e71eb85884d9aa6478f9ef6098c933f7f6070
Peter Hodges <peteredwardhodges@gmail.com>
2020-11-19 18:41:56 +0000

    Add winner state and most of turn behaviour

--- server/src/Game.ts
+++ server/src/Game.ts
@@ -1,3 +1,5 @@
+import { convertTypeAcquisitionFromJson, hasOnlyExpressionInitializer, textChangeRangeIsUnchanged } from "typescript";
+
 export enum Card {
     "A1", "A2", "B1", "B2", "C1", "C2", "D1", "D2", "E1", "E2", "F1", "F2", "BP"
 }
@@ -13,19 +15,22 @@ export const Deck: Card[] = [
 ];
 
 export interface Player {
+    id: number;
     name: string;
     cards: Card[];
 }
 
 export interface PendingPlayer {
+    id: number;
     name: string;
 }
 
 export interface GameState {
     id: string;
     started: boolean;
-    turn: Player;
+    turn: Player & {pickedCard: boolean};
     players: Player[];
+    winner?: Player;
 }
 
 export interface PendingGameState {
@@ -47,6 +52,7 @@ export const Game = {
             players: [
                 ...state.players,
                 {
+                    id: newId(state.players),
                     name,
                 }
             ]
@@ -62,7 +68,7 @@ export const Game = {
         const initCards = (player:PendingPlayer): Player => {
             return {
                 ...player,
-                cards: []
+                cards: [],
             }
         }
 
@@ -70,7 +76,7 @@ export const Game = {
             ...state,
             started: true,
             players: state.players.map(player => initCards(player)),
-            turn: initCards(state.players[0]),
+            turn: {...initCards(state.players[0]), pickedCard: false}
         }
 
         newState = dealCards(newState, shuffle(Deck));
@@ -81,19 +87,16 @@ export const Game = {
     selectCard: (state: GameState, player: Player, card: Card) =>  {
         
         // todo: Support player turns
-
+        // todo: ensure it only pushes states when actually required (fns should return falsy)
         if(player.name === state.turn.name) {
             // Player selecting own cards
             const newState = layCard(state, player, card);
-            if(newState) return newState;
+            if(newState) return checkWinner(checkTurn(newState));
         } 
         else {
             const newState = selectOpponentCard(state, player, card);
-            if(newState) return newState;
+            if(newState) return checkWinner(checkTurn(newState));
         }
-
-        // check for winner
-        // next turn
     },
 
 }
@@ -106,8 +109,6 @@ function dealCards(state: GameState, deck: Card[]): GameState {
         // Deal first card to player at index
         state.players[playerIndex] = {
             ...state.players[playerIndex],
-            // todo: look at this TS issue
-            // @ts-ignore;
             cards: state.players[playerIndex].cards.concat(deck[deckIndex])
         }
 
@@ -119,7 +120,7 @@ function dealCards(state: GameState, deck: Card[]): GameState {
     };
 }
 
-function layCard(state: GameState, player: Player, card: Card) {
+function layCard(state: GameState, player: Player, card: Card): GameState|false {
     // todo: Place cards in "playedCards" array for user
     if(card === Card.BP) return false;
 
@@ -147,14 +148,22 @@ function layCard(state: GameState, player: Player, card: Card) {
                     }
                 }
                 else { return p; }
-            })
+            }),
         };
     } 
+
+    return false;
 }
 
 function selectOpponentCard(state: GameState, player: Player, card: Card) {
+    if(state.turn.pickedCard) return state;
+
     return {
         ...state,
+        turn: {
+            ...state.turn,
+            pickedCard: true,
+        },
         players: state.players.map(p => {
             // Remove card from selected player
             if(p.name === player.name) {
@@ -188,4 +197,67 @@ function shuffle(array: any): any {
         array[j] = temp
     }   
     return array;
+}
+
+function checkTurn(state: GameState): GameState {
+    // Player must:
+    // - choose 1 x opponent card
+    // - play all pairs in their hand
+    const player = state.players.find(player => player.id === state.turn.id);
+
+    if(player) {
+        if(state.turn.pickedCard && !containsPair(player.cards)) {
+            return {
+                ...state,
+                turn: {...getNextPlayer(state, state.turn.id), pickedCard: false}
+            }
+        }
+    }
+
+    return state;
+}
+
+function checkWinner(state: GameState): GameState {
+    // Game is won when there's only one card left (and it's black peter)
+    let winner: Player|false = false;
+
+    for (const player of state.players) {
+        if(player.cards.length > 1) {
+            // At least one person has multiple cards - game is not won
+            return state;
+        }
+        if(player.cards.length === 1 && player.cards[0] === Card.BP) {
+            winner = player;
+        }
+    }
+
+    if(winner) {
+        return {
+            ...state,
+            winner: winner,
+        }
+    }
+
+    return state;
+}
+
+function getNextPlayer(state: GameState, playerId: number): Player {
+    // Find index of player
+    const index = state.players.findIndex(player => player.id === playerId);
+    if(index > -1) {
+        // Find index of next player (loop around array);
+        const newIndex = index < state.players.length-1 ? index+1 : 0;
+        return state.players[newIndex];
+    }
+    throw new Error("Could not find next player");
+}
+
+function containsPair(cards: Card[]): boolean {
+    // todo: Implement this
+    // How to determine duplicates? Probably need a better data structure for cards...
+    return false;
+}
+
+function newId(array: {id: number}[]): number {
+    return array.reduce((acc: number, curr: {id:number}) => acc === curr.id ? acc + 1 : acc, 0);
 }
